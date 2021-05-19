c2c272384eb48ec326d9270fef5ae7d4993c41a3
kardelendemiral <74200767+kardelendemiral@users.noreply.github.com>
2021-05-19 17:22:43 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,17 +2,33 @@ import sys
 
 def convert(o,a,op):
     opcode = int(o, 16)
-    addrmode = int(a, 16)
+   # addrmode = int(a, 16)
     operand = int(op, 16)
 
     bopcode = format(opcode, '06b')
-    baddrmode = format(addrmode, '02b')
+  #  baddrmode = format(addrmode, '02b')
     boperand = format(operand, '016b')
-    bin = '0b' + bopcode + baddrmode + boperand
-    ibin = int(bin[2:], 2);
+    bin = '0b' + bopcode + a + boperand
+    ibin = int(bin[2:], 2)
     instr = format(ibin, '06x')
     return instr
 
+def character_to_ascii(c):
+    return format(ord(c), "x")
+
+def register_num(ahmet):
+    if ahmet == "A":
+        return "1"
+    elif ahmet == "B":
+        return "2"
+    elif ahmet == "C":
+        return "3"
+    elif ahmet == "D":
+        return "4"
+    elif ahmet == "E":
+        return "5"
+    else:
+        return ahmet
 
 def bin_to_hex(s):
     decimal = int(s, 2)
@@ -22,12 +38,24 @@ def hex_to_bin(s):
     decimal = int(s,16)
     return bin(decimal)
 
+def ic(s):
+    leng = len(s)
+    ic = s[1: leng-1]
+    return ic
+
+def isnumeric(s):
+    for c in s:
+        if c>'9' or c<'0':
+            return False
+    return True
 
 inputFile = open(sys.argv[1], "r")
 #outputFile = open(sys.argv[1][0:sys.argv[1].index('.')]+".bin")
 outputFile = open("output", "w")
 variables = [None] * 65536
+map = {}
 memoryLoc = 0
+
 
 for line in inputFile:
     line = line.strip()
@@ -36,7 +64,7 @@ for line in inputFile:
 
     leng = len(str(tokens[0]))
     if tokens[0][leng-1] == ":":
-        variables[memoryLoc] = tokens[0][0:leng-1]
+        map.update({tokens[0][0:leng-1]:format(memoryLoc, '04x')})
     else:
         memoryLoc += 3
 
@@ -56,35 +84,37 @@ for line in inputFile:
     if tokens[0][leng-1] == ":":
         continue
     elif tokens[0]=="HALT":
-        o = "1"
+        outputFile.write("040000")
+        outputFile.write("\n")
+        continue
     elif tokens[0]=="LOAD":
-        o = "2"
+        o = "02"
     elif tokens[0] == "STORE":
-        o = "3"
+        o = "03"
     elif tokens[0] == "ADD":
-        o = "4"
+        o = "04"
     elif tokens[0] == "SUB":
-        o = "5"
+        o = "05"
     elif tokens[0] == "INC":
-        o = "6"
+        o = "06"
     elif tokens[0] == "DEC":
-        o = "7"
+        o = "07"
     elif tokens[0] == "XOR":
-        o = "8"
+        o = "08"
     elif tokens[0] == "AND":
-        o = "9"
+        o = "09"
     elif tokens[0] == "OR":
-        o = "A"
+        o = "0A"
     elif tokens[0] == "NOT":
-        o = "B"
+        o = "0B"
     elif tokens[0] == "SHL":
-        o = "C"
+        o = "0C"
     elif tokens[0] == "SHR":
-        o = "D"
+        o = "0D"
     elif tokens[0] == "NOP":
-        o = "E"
+        o = "0E"
     elif tokens[0] == "PUSH":
-        o = "F"
+        o = "0F"
     elif tokens[0] == "POP":
         o = "10"
     elif tokens[0] == "CMP":
@@ -112,16 +142,54 @@ for line in inputFile:
     elif tokens[0] == "PRINT":
         o = "1C"
 
-
     if len(tokens) == 2:
-        if isinstance(tokens[1], int) or tokens[1][0] == "'":
+        if isnumeric(tokens[1]) or tokens[1][0] == "'":
             a = "00"
-        elif tokens[1] == "A" or tokens[1] == "B" or tokens[1] == "C" or tokens[1] == "D":
+        elif tokens[1] == "A" or tokens[1] == "B" or tokens[1] == "C" or tokens[1] == "D" or tokens[1] == "E":
             a = "01"
-        elif tokens[1] == "[A]" or tokens[1] == "[B]" or tokens[1] == "[C]" or tokens[1] == "[D]":
+        elif tokens[1] == "[A]" or tokens[1] == "[B]" or tokens[1] == "[C]" or tokens[1] == "[D]" or tokens[1] == "[E]":
             a = "10"
         else:
             a = "11"
+
+
+
+    if a == "00":
+        if tokens[1][0] == "'":
+            op = character_to_ascii(tokens[1][1])
+        else:
+            op = tokens[1] #buraya MYDATA da gelebilir ama bizce gelmicek
+    elif a == "10":
+        ic = tokens[1][1]
+        op = register_num(ic)
+    elif a == "11":
+        if tokens[1][0] == "[":
+            ic = ic(tokens[1])
+            op = register_num(ic)
+        else:
+            op = map[tokens[1]]
+    elif a == "01":
+        op = register_num(tokens[1])
+
+    outputFile.write(convert(o,a,op))
+    outputFile.write("\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
