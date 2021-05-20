d4eecd14e313075d27dd38b37183555583603969
Kardelen Erdal <47900788+kardelenerdal@users.noreply.github.com>
2021-05-20 23:34:24 +0300

    Update exec.py

--- exec.py
+++ exec.py
@@ -1,3 +1,20 @@
+# set_cf set_zf set_sf
+def store(operand, add_mode):   # operand and add_mode can be (B)0002-01 ,([B])0002-10, xxxx-11
+    decimal_address = int(operand, 16)
+    value_to_be_stored = registers[0]
+    if add_mode == '01':
+        reg_name = get_register_name(operand)        # A, B falan oldu
+        registers[reg_name-'A'+1] = value_to_be_stored
+    elif add_mode == '10':
+        reg_name = get_register_name(operand)        # A, B falan oldu
+        store_address = registers[reg_name-'A'+1]    # reg deki adresi aldim
+        store_dec_add = int(store_address, 16)
+        memory[store_dec_add] = value_to_be_stored   # regdeki adrese koydum
+    elif add_mode == '11':
+        memory[decimal_address] = value_to_be_stored # direkt o adrese koydum    
+
+
+
 def get_value(addmode, operand):
     if addmode == "00":
         return operand
@@ -106,13 +123,16 @@ inputFile = open("output", "r")
 outputFile = open("output2", "w")
 
 instructions = []
+reg_max_value = 31
 
 for line in inputFile:
     instructions.append(line)
 
-instruction = instructions[0]
 
-registers = [0, 0, 0, 0, 0, 0, 0]
+isJump = False
+inst_count = 0
+instruction = instructions[inst_count]
+registers = [0, 0, 0, 0, 0, 0, 0]  # PC A B C D E S bunlar decimal depoluyo
 memory = [None] * 65536
 stack = []
 cf = False
@@ -120,72 +140,240 @@ zf = False
 sf = False
 
 while True:  # bu boyle cunku kac intructionlari kac kere execute edicegimizi bilmiyoz halt gelene kadar bakcaz
+    isJump = False
     first = instruction[0:2]
     ia = get_inst_add_type(first)
     inst_type = ia[0]
     addressing_mode = ia[1]
     last = instruction[2:]
-    value = get_value(addressing_mode, last)
-
+    value = int(get_value(addressing_mode, last), 16) # decimal dondu
+    
     dumb = 0
 
     if first == "HALT":
         break
-    elif inst_type == 'LOAD':
-        a = value
+    elif inst_type == 'LOAD': # 0041
+        registers[1] = value
     elif inst_type == 'STORE':
-        # store(value) bu funcda a'yi store edicegimiz yeri bulup store ederiz
+        store(last, addressing_mode) #bu funcda a'yi store edicegimiz yeri bulup store ederiz
         dumb = 0
     elif inst_type == 'ADD':
         # cf, sf, zf
-        a = a + value
-        # flaglari set et
+        registers[1] += value
+        if cf:
+            registers[1] += reg_max_value + 1
+        if registers[1] < 0:
+            sf = True
+        else:
+            sf = False
+        if registers[1] == 0:
+            zf = True
+        else:
+            zf = False    
+        if registers[1] > reg_max_value:
+            cf = True
+        else:
+            cf = False    
     elif inst_type == 'SUB':
         # cf, sf, zf
-        a = a - value
-        # flaglar
-    elif inst_type == 'INC':
+        registers[1] -= value
+        if cf:
+            registers[1] += reg_max_value + 1
+        if registers[1] < 0:
+            sf = True
+        else:
+            sf = False
+        if registers[1] == 0:
+            zf = True
+        else:
+            zf = False    
+        if registers[1] > reg_max_value:
+            cf = True
+        else:
+            cf = False
+    elif inst_type == 'INC':        # INC 0041'A' INC 0002(B) registers[2] +=1   INC[B] memory[45]+=1  B=45 INC[xxxx] memory[xdec] +=1
         # cf, sf, zf
-        # value'yu arttircaz
-        dumb = 0
+        # flag varsa bakmamıza gerek var mı bilmiyoruz
+        result = 0
+        if addressing_mode == '01':
+            reg_name = get_register_name(last)  
+            registers[reg_name-'A'+1] += 1
+            result = registers[reg_name-'A'+1]
+        elif addressing_mode == '00':
+            dec_value = int(last, 16)
+            result = dec_value + 1
+        elif addressing_mode == '10':
+            lastChar = last[-1]
+            reg = lastChar - '0'
+            memoryLoc = registers[reg]
+            memory[memoryLoc] += 1
+            result = memory[memoryLoc]
+        elif addressing_mode == '11':
+            dec_value = int(last, 16)
+            memory[dec_value] += 1
+            result = memory[dec_value]
+
+        
+        if result < 0:
+            sf = True
+        else:
+            sf = False
+        if result == 0:
+            zf = True
+        else:
+            zf = False    
+        if result > reg_max_value:
+            cf = True
+        else:
+            cf = False
+
     elif inst_type == 'DEC':
         # cf, sf, zf
-        dumb = 0
+        result = 0
+        if addressing_mode == '01':
+            reg_name = get_register_name(last)  
+            registers[reg_name-'A'+1] -= 1
+            result = registers[reg_name-'A'+1]
+        elif addressing_mode == '00':
+            dec_value = int(last, 16)
+            result = dec_value - 1
+        elif addressing_mode == '10':
+            lastChar = last[-1]
+            reg = lastChar - '0'
+            memoryLoc = registers[reg]
+            memory[memoryLoc] -= 1
+            result = memory[memoryLoc]
+        elif addressing_mode == '11':
+            dec_value = int(last, 16)
+            memory[dec_value] -= 1
+            result = memory[dec_value]
+
+        
+        if result < 0:
+            sf = True
+        else:
+            sf = False
+        if result == 0:
+            zf = True
+        else:
+            zf = False    
+        if result > reg_max_value:
+            cf = True
+        else:
+            cf = False
     elif inst_type == 'XOR':
         # sf, zf
-        a = a ^ value
-        # flaglar
+        a_dec_value = registers[1]
+        result = value ^ a_dec_value
+        registers[1] = result
+
+        if result < 0:
+            sf = True
+        else:
+            sf = False
+        if result == 0:
+            zf = True
+        else:
+            zf = False 
     elif inst_type == 'AND':
         # sf, zf
-        a = a and value  # ?????cidden boyle mi
+        a_dec_value = registers[1]
+        result = value & a_dec_value
+        registers[1] = result
+
+        if result < 0:
+            sf = True
+        else:
+            sf = False
+        if result == 0:
+            zf = True
+        else:
+            zf = False
     elif inst_type == 'OR':
         # sf, zf
-        a = a or value
+        a_dec_value = registers[1]
+        result = value | a_dec_value
+        registers[1] = result
+
+        if result < 0:
+            sf = True
+        else:
+            sf = False
+        if result == 0:
+            zf = True
+        else:
+            zf = False
     elif inst_type == 'NOT':
-        # sf, zf
-        dumb = 0
-        # not'ini alip napicaz yazmamis
+        result = ~value
+        if result < 0:
+            sf = True
+        else:
+            sf = False
+        if result == 0:
+            zf = True
+        else:
+            zf = False
     elif inst_type == 'SHL':
         # cf, sf, zf
-        dumb = 0
-    # value'daki registeri << yapcak
+        result = value << 1
+
+        if result < 0:
+            sf = True
+        else:
+            sf = False
+        if result == 0:
+            zf = True
+        else:
+            zf = False
+        if result > reg_max_value:
+            cf = True
+        else:
+            cf = False    
+    
     elif inst_type == 'SHR':
         # sf, zf
-        # register'i >> yapcak
-        dumb = 0
+        result = value >> 1
+        
+        if result < 0:
+            sf = True
+        else:
+            sf = False
+        if result == 0:
+            zf = True
+        else:
+            zf = False
+
     elif inst_type == 'NOP':
         continue
     elif inst_type == 'PUSH':
         # Push a word sized operand (two bytes) and update S by subtracting 2.
-        dumb = 0
+        stack.append(value)
+        registers[6] -= 2 # bununla napıcaz
     elif inst_type == 'POP':
         # Pop a word sized data (two bytes) into the operand and update S by adding 2.
-        dumb = 0
+        lastChar = last[-1]
+        reg = lastChar - '0'
+        registers[reg] = stack.pop()
+        registers[6] += 2 # bununla napıcaz
     elif inst_type == 'CMP':
         # cf, sf, zf
         # Perform comparison (AC-operand) and set flag accordingly.
-        dumb = 0
-    elif inst_type == 'JMP':
+        a_value = registers[1]
+        result = a_value - value
+        if result< 0:
+            sf = True
+        else:
+            sf = False
+        if result == 0:
+            zf = True
+        else:
+            zf = False    
+        if result > reg_max_value:
+            cf = True
+        else:
+            cf = False     
+
+    elif inst_type == 'JMP': # last'ı decimala çevir ve 3'e bol sonra inst_count = orası
         # Unconditional jump. Set PC to address.
         dumb = 0
     elif inst_type == 'JZ':
@@ -213,8 +401,25 @@ while True:  # bu boyle cunku kac intructionlari kac kere execute edicegimizi bi
         # Conditional jump. Jump if below or equal
         dumb = 0
     elif inst_type == 'READ':
-        # Reads	a character into the operand.
-        dumb = 0
+        # Reads a character into the operand.
+        read_char = input()
+        result = ord(read_char)
+        if addressing_mode == '01':
+            reg_name = get_register_name(last)  
+            registers[reg_name-'A'+1] = result
+        elif addressing_mode == '10':
+            lastChar = last[-1]
+            reg = lastChar - '0'
+            memoryLoc = registers[reg]
+            memory[memoryLoc] = result
+        elif addressing_mode == '11':
+            dec_value = int(last, 16)
+            memory[dec_value] = result
+
     elif inst_type == 'PRINT':
         # Prints the operand as a character. abi zaten char yazin diyomus biz malmisiz
-        dumb = 0
+        print(chr(value))
+
+    
+    if not isJump:
+        inst_count += 1
