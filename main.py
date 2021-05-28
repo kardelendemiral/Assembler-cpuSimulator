c7f5db65fb2ddcf23ae2ee7426710c1ba8c9636f
Kardelen Erdal <47900788+kardelenerdal@users.noreply.github.com>
2021-05-28 16:51:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def character_to_ascii(c):
     return format(ord(c), "x")
 
 def register_num(ahmet):
-    if ahmet == "A" or ahmet == "a" :
+    if ahmet == "A" or ahmet == "a":
         return "1"
     elif ahmet == "B" or ahmet == "b":
         return "2"
@@ -27,6 +27,10 @@ def register_num(ahmet):
         return "4"
     elif ahmet == "E" or ahmet == "e":
         return "5"
+    elif ahmet == "PC" or ahmet == "pc":
+        return "0"
+    elif ahmet == "S" or ahmet == "s":
+        return "6"        
     else:
         return ahmet
 
@@ -39,8 +43,7 @@ def hex_to_bin(s):
     return bin(decimal)
 
 def ishexnum(s):  # ffdc
-    
-    if s.upper() == 'A' or s.upper() == 'B' or s.upper() == 'C' or s.upper() == 'D' or s.upper() == 'E':
+    if not isnumeric(s[0]):
         return False
     for x in s:
         if (x>'9' or x<'0') and (x>'F' or x<'A') and (x>'f' or x<'a'):
@@ -67,6 +70,7 @@ def removeSpaces(s):
         s = s[0:leng-1]
 
     return s
+
 def checkLine(line):
     i = len(line) - 1
     if line[-1] == ':':
@@ -86,7 +90,7 @@ def checkLine(line):
         if not(tokens[0] == "HALT") and not(len(tokens) == 2):
             print("invalid instruction")
             exit()
-    
+
 
 inputFile = open(sys.argv[1], "r")
 outputFile = open(sys.argv[1][0:sys.argv[1].index('.')]+".bin", "w")
@@ -104,8 +108,8 @@ for line in inputFile:
     #print str(tokens)
 
     leng = len(str(tokens[0]))
-    if tokens[0][leng-1] == ":":
-        myMap.update({tokens[0][0:leng-1].upper():format(memoryLoc, '04x')})
+    if tokens[0][leng-1] == ":" and not isnumeric(tokens[0][0]):
+        myMap.update({tokens[0][0:leng-1]:format(memoryLoc, '04x')})
     else:
         memoryLoc += 3
 
@@ -113,89 +117,93 @@ inputFile.close()
 inputFile = open(sys.argv[1], "r")
 
 for line in inputFile:
-
     if line.isspace():
         continue
 
     line = line.strip()
     tokens = line.split()
+    
+    if len(tokens) == 3:
+        if (tokens[1] == "'" and tokens[2] == "'") or (tokens[1] == '"' and tokens[2] == '"'):
+            tokens = [tokens[0], "' '"]
+
     str = ""
     o = ""
     a = ""
     op = ""
     leng = len(tokens[0])
-
+    tokens[0] = tokens[0].upper()
 
     if tokens[0][leng-1] == ":":
         continue
-    elif tokens[0].upper()=="HALT":
+    elif tokens[0]=="HALT":
         outputFile.write("040000")
         outputFile.write("\n")
         continue
-    elif tokens[0].upper()=="LOAD":
+    elif tokens[0]=="LOAD":
         o = "02"
-    elif tokens[0].upper() == "STORE":
+    elif tokens[0] == "STORE":
         o = "03"
-    elif tokens[0].upper() == "ADD":
+    elif tokens[0] == "ADD":
         o = "04"
-    elif tokens[0].upper() == "SUB":
+    elif tokens[0] == "SUB":
         o = "05"
-    elif tokens[0].upper() == "INC":
+    elif tokens[0] == "INC":
         o = "06"
-    elif tokens[0].upper() == "DEC":
+    elif tokens[0] == "DEC":
         o = "07"
-    elif tokens[0].upper() == "XOR":
+    elif tokens[0] == "XOR":
         o = "08"
-    elif tokens[0].upper() == "AND":
+    elif tokens[0] == "AND":
         o = "09"
-    elif tokens[0].upper() == "OR":
+    elif tokens[0] == "OR":
         o = "0A"
-    elif tokens[0].upper() == "NOT":
+    elif tokens[0] == "NOT":
         o = "0B"
-    elif tokens[0].upper() == "SHL":
+    elif tokens[0] == "SHL":
         o = "0C"
-    elif tokens[0].upper() == "SHR":
+    elif tokens[0] == "SHR":
         o = "0D"
-    elif tokens[0].upper() == "NOP":
+    elif tokens[0] == "NOP":
         o = "0E"
-    elif tokens[0].upper() == "PUSH":
+    elif tokens[0] == "PUSH":
         o = "0F"
-    elif tokens[0].upper() == "POP":
+    elif tokens[0] == "POP":
         o = "10"
-    elif tokens[0].upper() == "CMP":
+    elif tokens[0] == "CMP":
         o = "11"
-    elif tokens[0].upper() == "JMP":
+    elif tokens[0] == "JMP":
         o = "12"
-    elif tokens[0].upper() == "JZ" or tokens[0].upper() == "JE":
+    elif tokens[0] == "JZ" or tokens[0] == "JE":
         o = "13"
-    elif tokens[0].upper() == "JNZ" or tokens[0].upper() == "JNE":
+    elif tokens[0] == "JNZ" or tokens[0] == "JNE":
         o = "14"
-    elif tokens[0].upper() == "JC":
+    elif tokens[0] == "JC":
         o = "15"
-    elif tokens[0].upper() == "JNC":
+    elif tokens[0] == "JNC":
         o = "16"
-    elif tokens[0].upper() == "JA":
+    elif tokens[0] == "JA":
         o = "17"
-    elif tokens[0].upper() == "JAE":
+    elif tokens[0] == "JAE":
         o = "18"
-    elif tokens[0].upper() == "JB":
+    elif tokens[0] == "JB":
         o = "19"
-    elif tokens[0].upper() == "JBE":
+    elif tokens[0] == "JBE":
         o = "1A"
-    elif tokens[0].upper() == "READ":
+    elif tokens[0] == "READ":
         o = "1B"
-    elif tokens[0].upper()== "PRINT":
+    elif tokens[0] == "PRINT":
         o = "1C"
     else:
         print("invalid instruction")
-        exit()
+        exit()    
 
-
-    if isnumeric(tokens[1]) or tokens[1][0] == "'" or (tokens[1].upper() in myMap) or ishexnum(tokens[1]):
+    #tokens[1] = tokens[1].upper()
+    if isnumeric(tokens[1]) or tokens[1][0] == "'" or (tokens[1].upper() in myMap) or ishexnum(tokens[1]) or tokens[1][0] == '"':
         a = "00"
-    elif tokens[1] == "A" or tokens[1] == "B" or tokens[1] == "C" or tokens[1] == "D" or tokens[1] == "E" or tokens[1] == "a" or tokens[1] == "b" or tokens[1] == "c" or tokens[1] == "d" or tokens[1] == "e":
+    elif tokens[1].upper() == "A" or tokens[1].upper() == "B" or tokens[1].upper() == "C" or tokens[1].upper() == "D" or tokens[1].upper() == "E" or tokens[1].upper() == "PC" or tokens[1].upper() == "S":
         a = "01"
-    elif tokens[1] == "[A]" or tokens[1] == "[B]" or tokens[1] == "[C]" or tokens[1] == "[D]" or tokens[1] == "[E]" or tokens[1] == "[a]" or tokens[1] == "[b]" or tokens[1] == "[c]" or tokens[1] == "[d]" or tokens[1] == "[e]":
+    elif tokens[1].upper() == "[A]" or tokens[1].upper() == "[B]" or tokens[1].upper() == "[C]" or tokens[1].upper() == "[D]" or tokens[1].upper() == "[E]" or tokens[1].upper() == "[PC]" or tokens[1].upper() == "[S]":
         a = "10"
     else:
         a = "11"
@@ -203,25 +211,45 @@ for line in inputFile:
 
 
     if a == "00":
-        if tokens[1][0] == "'" and tokens[1][2] == "'":
+        if (tokens[1][0] == "'" and tokens[1][2] == "'") or (tokens[1][0] == '"' and tokens[1][2] == '"') :
             op = character_to_ascii(tokens[1][1])
-        elif (tokens[1].upper() in myMap):
-            op = myMap[tokens[1].upper()]
+        elif (tokens[1].upper() in myMap and not isnumeric(tokens[1][0])):
+            op = myMap[tokens[1]]
         else:
-            op = tokens[1] #ffdc
+            while len(tokens[1]) < 4:
+                tokens[1] = '0' + tokens[1]
+            if len(tokens[1]) > 4:
+                while len(tokens[1]) > 4:
+                    if tokens[1][0] == '0':
+                        tokens[1] = tokens[1][1:]
+                    else:
+                        print("wrong immediate data")
+                        exit()    
+            op = tokens[1]
+    
     elif a == "10":
-        ic = tokens[1][1]
+        ic = tokens[1][1:-1]
         op = register_num(ic)
+
     elif a == "11":
-        if tokens[1][0] == "[" and tokens[1][-1] == "]" and len(tokens[1]) == 6:
+        if tokens[1][0] == "[" and tokens[1][-1] == "]":
             ic = ic(tokens[1])
+            while len(ic) < 4:
+                ic = '0' + ic
+            if len(ic) > 4:
+                while len(ic) > 4:
+                    if ic[0] == '0':
+                        ic = ic[1:]
+                    else:
+                        print("wrong address")
+                        exit()
             op = register_num(ic)
+    
     elif a == "01":
         op = register_num(tokens[1])
 
-
     if len(o) == 0 or len(a) == 0 or len(op) == 0:
         print("syntax error")
-        exit()
+        exit()    
     outputFile.write(convert(o,a,op))
     outputFile.write("\n")
